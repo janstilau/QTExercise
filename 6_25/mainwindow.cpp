bb32b8e7401575cec96bf88dcad49a98581b38f5
justin lau <janstilau@gmail.com>
2018-06-26 23:43:40 +0800

    sync

--- 6_25/mainwindow.cpp
+++ 6_25/mainwindow.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <map>
 #include <set>
+#include <QDebug>
 
 using namespace std;
 
@@ -56,6 +57,243 @@ ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
     return head;
 }
 
-ListNode* mergeKLists(vector<ListNode*>& lists) {
+//int findSmall(vector<ListNode*>& lists) {
+
+//    int index = -1;
+//    int min = INT_MAX;
+//    for (int i = 0; i < lists.size(); ++i) {
+//        ListNode *current = lists[i];
+//        if (!current) {
+//            continue;
+//        }
+//        if (current->val < min) {
+//            index = i;
+//            min = current->val;
+//        }
+//    }
+//    return index;
+//}
+
+//ListNode* mergeKLists(vector<ListNode*>& lists) {
+//    vector<ListNode*> backup = lists;
+
+//    ListNode temp = ListNode(-1);
+//    ListNode *current = &temp;
+
+//    int index = findSmall(backup);
+//    while (index != -1) {
+//        ListNode *node =  backup[index];
+//        current->next = node;
+//        current = current->next;
+//        backup[index] = node->next;
+//        index = findSmall(backup);
+//    }
+
+//    return temp.next;
+//}
+
+//ListNode* mergeKLists(vector<ListNode*>& lists)
+//{
+
+//    int size = lists.size();
+//    if (size == 0) { return NULL;}
+//    if (size == 1) { return lists[0]; }
+//    if (size == 2) {
+//        return mergeTwoLists(list[0], list[1]);
+//    }
+
+//    int mid = size / 2;
+//    vector<ListNode*> sub_1;
+//    vector<ListNode*> sub_2;
+//    for (int i = 0; i < mid; ++i) {
+//        sub_1.push_back(lists[i]);
+//    }
+//    for (int i = mid; i < size; ++i) {
+//        sub_2.push_back(lists[i]);
+//    }
+
+//    ListNode* sub_1Node = mergeKLists(sub_1);
+//    ListNode* sub_2Node = mergeKLists(sub_2);
+//    mergeTwoLists(sub_1Node, sub_2Node);
+//}
+
+
+class MyStack {
+public:
+    /** Initialize your data structure here. */
+    MyStack() {
+
+    }
+
+    /** Push element x onto stack. */
+    void push(int x) {
+        queue<int> temp;
+        temp.push(x);
+        while (!mQueue.empty()) {
+            temp.push(mQueue.front());
+            mQueue.pop();
+        }
+        while (!temp.empty()) {
+           mQueue.push(temp.front());
+           temp.pop();
+        }
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int value = mQueue.front();
+        mQueue.pop();
+        return value;
+    }
+
+    /** Get the top element. */
+    int top() {
+        return mQueue.front();
+    }
+
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return mQueue.empty();
+    }
+
+private:
+    queue<int> mQueue;
+};
+
+
+class MyQueue {
+public:
+    /** Initialize your data structure here. */
+    MyQueue() {
+
+    }
+
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        mStack.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        if (mReverseStack.empty()) {
+            while (!mStack.empty()) {
+               mReverseStack.push(mStack.top());
+               mStack.pop();
+            }
+        }
+
+        int value = mReverseStack.top();
+        mReverseStack.pop();
+        return value;
+    }
+
+    /** Get the front element. */
+    int peek() {
+        if (mReverseStack.empty()) {
+            while (!mStack.empty()) {
+               mReverseStack.push(mStack.top());
+               mStack.pop();
+            }
+        }
+        return mReverseStack.top();
+    }
+
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return mStack.empty() && mReverseStack.empty();
+    }
+
+private:
+
+    stack<int> mStack;
+    stack<int> mReverseStack;
+};
+
+
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+        mMin = INT_MAX;
+    }
+
+    void push(int x) {
+        if (x < mMin) { mMin = x;}
+        mStack.push(x);
+        mMinStack.push(mMin);
+    }
+
+    void pop() {
+        mStack.pop();
+        mMinStack.pop();
+        if (mMinStack.empty()) {
+            mMin = INT_MAX;
+        }
+    }
+
+    int top() {
+        return mStack.top();
+    }
+
+    int getMin() {
+        return mMinStack.top();
+    }
+
+private:
+    int mMin;
+    stack<int> mStack;
+    stack<int> mMinStack;
+};
+
+
+bool check_is_valid_order(queue<int> &order)
+{
+    stack<int> stash;
 
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    MinStack stack;
+    stack.push(INT_MAX - 1);
+    stack.push(INT_MAX - 1);
+    stack.push(INT_MAX);
+    stack.pop();
+    stack.pop();
+    stack.pop();
+    stack.push(INT_MAX);
+    stack.top();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
